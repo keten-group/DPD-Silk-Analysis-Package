2028b5890c58573f2de93ae274f0233ee36908dd
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2023-07-28 15:56:33 -0500

    Modified run_all_network_conductance.sh

--- Simulation_Analysis/run_all_network_conductance.sh
+++ Simulation_Analysis/run_all_network_conductance.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 module load matlab
-for i in {1..14..1} # Change loop length to reflect the number of frames in equil_{randomseed}.dcd.
+for i in {0..7..1} # Change this to loop through the number of frames in equil_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp Network_Conductance.m equil_evolve_$i
     cd equil_evolve_$i
-    matlab -nodisplay -r "Network_Conductance ; exit"
-    cd ..
-  done
-
-for i in {0..28..1} # Change loop length to reflect the number of frames in shear_{randomseed}.dcd.
-  do
-    cp Network_Conductance.m shear_evolve_$i
-    cd shear_evolve_$i
     matlab -nodisplay -r "Network_Conductance ; exit" # From: https://arc.umich.edu/software/matlab/#:~:text=To%20run%20a%20MATLAB%20script,m%20from%20the%20current%20directory.&text=Note%20that%20the%20MATLAB%20script,an%20exit%20command%20in%20it.
     cd ..
   done
 
-for i in {0..5..1} # Change loop length to reflect the number of frames in stretch_{randomseed}.dcd.
+for i in {0..14..1} # Change this to loop through the number of frames in shear_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
+  do
+    cp Network_Conductance.m shear_evolve_$i
+    cd shear_evolve_$i
+    matlab -nodisplay -r "Network_Conductance ; exit" 
+    cd ..
+  done
+
+for i in {0..5..1} # Change this to loop through the number of frames in stretch_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp Network_Conductance.m stretch_evolve_$i
     cd stretch_evolve_$i
