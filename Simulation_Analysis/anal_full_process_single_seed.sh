20e71bbd17b65e3c6623cd988d9287e78432d988
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2023-07-28 15:08:02 -0500

    Changed loops in run_all_node_bridge_multiplicity.sh and anal_full_process_single_seed.sh

--- Simulation_Analysis/anal_full_process_single_seed.sh
+++ Simulation_Analysis/anal_full_process_single_seed.sh
@@ -10,7 +10,7 @@ module load octave # Load octave on a Linux machine under bash.
 module load python/anaconda3.6 # Load python/anaconda3.6 on a Linux machine under bash.
 octave separate_coordinate_single.m
 
-for i in {0..7..1} # Change this to loop through the number of frames in equil_{randomseed}.dcd
+for i in {0..7..1} # Change this to loop through the number of frames in equil_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp DFS.m Nbead.txt network_noprint.py get_coordinate.m get_connectivity.m equil_evolve_$i
     cd equil_evolve_$i
@@ -20,7 +20,7 @@ for i in {0..7..1} # Change this to loop through the number of frames in equil_{
     cd ..
   done
 
-for i in {0..14..1} # Change this to loop through the number of frames in shear_{randomseed}.dcd
+for i in {0..14..1} # Change this to loop through the number of frames in shear_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp DFS.m Nbead.txt network_noprint.py get_coordinate.m get_connectivity.m shear_evolve_$i
     cd shear_evolve_$i
@@ -30,7 +30,7 @@ for i in {0..14..1} # Change this to loop through the number of frames in shear_
     cd ..
   done
 
-for i in {0..5..1} # Change this to loop through the number of frames in stretch_{randomseed}.dcd
+for i in {0..5..1} # Change this to loop through the number of frames in stretch_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp DFS.m Nbead.txt network_noprint.py get_coordinate.m get_connectivity.m stretch_evolve_$i
     cd stretch_evolve_$i
--- Simulation_Analysis/run_all_node_bridge_multiplicity.sh
+++ Simulation_Analysis/run_all_node_bridge_multiplicity.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 module load matlab
-for i in {1..14..1} # Change this loop length to reflect the number of frames in equil_{randomseed}.dcd.
+for i in {0..7..1} # Change this to loop through the number of frames in equil_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp node_bridge_diagram_multiplicity.m equil_evolve_$i
     cd equil_evolve_$i
-    matlab -nodisplay -r "node_bridge_diagram_multiplicity ; exit"
-    cd ..
-  done
-
-for i in {0..28..1} # Change this loop length to reflect the number of frames in shear_{randomseed}.dcd.
-  do
-    cp node_bridge_diagram_multiplicity.m shear_evolve_$i
-    cd shear_evolve_$i
     matlab -nodisplay -r "node_bridge_diagram_multiplicity ; exit" # From: https://arc.umich.edu/software/matlab/#:~:text=To%20run%20a%20MATLAB%20script,m%20from%20the%20current%20directory.&text=Note%20that%20the%20MATLAB%20script,an%20exit%20command%20in%20it.
     cd ..
   done
 
-for i in {0..5..1} # Change this loop length to reflect the number of frames in stretch_{randomseed}.dcd.
+for i in {0..14..1} # Change this to loop through the number of frames in shear_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
+  do
+    cp node_bridge_diagram_multiplicity.m shear_evolve_$i
+    cd shear_evolve_$i
+    matlab -nodisplay -r "node_bridge_diagram_multiplicity ; exit" 
+    cd ..
+  done
+
+for i in {0..5..1} # Change this to loop through the number of frames in stretch_{randomseed}.dcd divided by the -stride specified by the ./catdcd command above.
   do
     cp node_bridge_diagram_multiplicity.m stretch_evolve_$i
     cd stretch_evolve_$i
