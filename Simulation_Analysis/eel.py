c4dfb7a3934af7d5745716537823803ca0342c58
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2024-04-11 17:01:51 -0500

    Added eel.py and modifed orderpara.py to calculate values through entire simulation rather than just the processing step.

--- Simulation_Analysis/eel.py
+++ Simulation_Analysis/eel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Import necessary libraries.
 import MDAnalysis as mda
 from MDAnalysis import transformations
 import numpy as np
@@ -14,16 +15,17 @@ from matplotlib import colors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.ticker as plticker
 
-def order_his(u, sid):
+# Define a function which calculates the end to end length of each individual chain in a specified universe.
+
+def eel_his(u, sid):
     
-    shis = []
-
+    eelhis = []
+    
     for ts in u.trajectory:
-        temppos = u.select_atoms(f"segid {sid}").positions[1:]-u.select_atoms(f"segid {sid}").positions[:-1]
-        temppos = temppos/np.linalg.norm(temppos,axis=1).reshape([len(temppos),1])
-        shis.append(0.5*(3*np.mean((np.inner(temppos, np.array([1,0,0])))**2)-1))
-
-    return np.array(shis)
+        position_1 = u.select_atoms(f"segid {sid}").positions[0]
+        position_2 = u.select_atoms(f"segid {sid}").positions[-1]
+        eelhis.append(mda.lib.distances.distance_array(position_1, position_2)[0][0])
+    return eelhis
 
 # Set simulation-specific variables specifying random seed used and dcd files to be analyzed.
 randseed = '11111'
@@ -42,18 +44,19 @@ u = mda.Universe(f'{psf_protein_only}',
 # For pull simulation. Uncomment the below universe command and comment the above universe command.
 # Load all dcd files and protein only psf into an MDAnalysis universe.
 # u = mda.Universe(f'{psf_protein_only}', 
-#                 [f'{equil_dcd}',f'{pull_dcd}',f'{relax_dcd}',f'{tensile_dcd}'],in_memory=True)
+                # [f'{equil_dcd}',f'{pull_dcd}',f'{relax_dcd}',f'{tensile_dcd}'],in_memory=True)
 
-# Calculate order parameter using the function order_his.
+# Calculate end to end length using the function eel_his.
 nofr = len(u.trajectory) # Number of frames in the trajectory
 # nochain = int(u.atoms[-1].segid) # Number of chains in the trajectory
-nochain = 100 # Reset nochain to only analyze the first 100 chains.
-len_his = np.zeros([nochain, nofr]) # History of all chains in the trajectory. Reset to only analyze the first 100 chains.
+nochain = 100 # Reset nochain to only analyze the first 10 chains.
+len_his = np.zeros([nochain, nofr]) # History of all chains in the trajectory. Reset to only analyze the first 10 chains.
 for ss in range(1,nochain+1):
-    len_his[ss-1] = order_his(u, ss)
+    len_his[ss-1] = eel_his(u, ss)
     print(ss)
+pkl.dump(len_his, open(f'eel_his.pkl','wb'))
 
-# Compute mean and standard deviation of order parameter at each frame in the simulation then plot.
+# Compute mean and standard deviation of end to end length at each frame in the simulation then plot.
 mean = np.mean(len_his, axis=0)
 
 std = np.std(len_his, axis=0)
@@ -62,12 +65,10 @@ fig = plt.figure(figsize=[4,3])
 ax = fig.add_subplot(111)
 plt.errorbar(np.arange(nofr), mean, std, linestyle='None', marker='^')
 plt.xlabel('frame no.')
-plt.ylabel(f"Herman's Order")
+plt.ylabel(f"End to End Length")
 plt.tight_layout()
-plt.savefig(f'order_his.png', dpi=200)
+plt.savefig(f'eel_his.png', dpi=200)
 plt.close()
-
-pkl.dump(len_his, open(f'order_his.pkl','wb'))
 
 
 
